56875aff87056ce24a90c28535a940d6f4e8ba1d
venkey45R <venkateshamulraj@gmail.com>
2024-07-28 10:57:01 +0530

    added a tags

--- src/assets/Contact.jsx
+++ src/assets/Contact.jsx
@@ -6,7 +6,9 @@ function Contact() {
     <div className='pb-20 border-b border-neutral-900'>
         <motion.h1 whileInView={{opacity: 1, y:0}} initial={{opacity: 0, y:-100}} transition={{duration: 0.5}} className='my-10 text-4xl text-center '>Get In Touch</motion.h1>
         <motion.div whileInView={{opacity: 1, y:0}} initial={{opacity: 0, y: 50}} transition={{duration: 1}} className='tracking-tighter text-center '>
+          <a href='mailto:venkateshamulraj@gmail.com'>
             <p className='my-4'>venkateshamulraj@gmail.com</p>
+          </a>
             <p className='my-4'>+91_9342972725</p>
         </motion.div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {FaLinkedin} from "react-icons/fa";
 import {FaGithub} from "react-icons/fa";
 import {FaSquareXTwitter} from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -10,9 +11,15 @@ function Navbar() {
             <h1 className='text-xl font-bold '>Ve</h1>
         </div>
         <div className='flex items-center justify-center gap-4 mx-8 mt-8 mb-0 text-2xl'>
-            <FaLinkedin />
-            <FaGithub />
-            <FaSquareXTwitter />
+            <a href="https://www.linkedin.com/in/venkateshamulraj2004" target='_blank'>
+              <FaLinkedin />
+            </a>       
+            <a href='https://github.com/Venkey45R' target='_blank'>   
+              <FaGithub />
+            </a>
+            <a href='https://x.com/Venkatesh_4510' target='_blank'>   
+              <FaSquareXTwitter />
+            </a>
         </div>
     </nav>
   )
